149935cb995cc6a424e456d1fdcfeead3106a08b
KirilZal <kirilzalov@gmail.com>
2024-05-23 17:43:40 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace Task1
+
+{
+     class Program
+    {
+        static AutoResetEvent auto = new AutoResetEvent(false);
+
+        static void Fun1()
+        {
+            Console.WriteLine("push1");
+            auto.WaitOne();
+            Console.WriteLine("finish1");
+        }
+        static void Fun2()
+        {
+            Console.WriteLine("push2");
+            auto.WaitOne();
+            Console.WriteLine("finish2");
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding=Encoding.Unicode;
+           Thread thread1= new Thread(Fun1);
+            Thread thread2= new Thread(Fun2);
+            thread1.Start();
+            thread2.Start();
+
+            Thread.Sleep(1000);
+            Console.WriteLine("pushing loading");
+           
+            auto.Set();
+            auto.Set();
+            thread1.Join();
+            thread2.Join();
+            Console.WriteLine("finitoooo");
+            
+        }
+    }
+
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Threading;
+
+namespace Task2
+{
+    class Program
+    {
+        private static readonly string myMutex = "MyUniqueMutex";
+        private static Mutex mutex;
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            bool creatNew = false;
+            try
+            {
+
+                mutex = new Mutex(false, myMutex, out creatNew);
+                if (!creatNew)
+                {
+                    Console.WriteLine("матекс зайнятий");
+                    return;
+                }
+                Console.WriteLine("виконуется програма");
+                Console.ReadLine();
+                
+               
+            }
+            finally
+            {
+                if (creatNew && mutex != null)
+                {
+                    mutex.Dispose();
+                }
+             
+
+            }
+
+            
+            
+        }
+    }
+}
